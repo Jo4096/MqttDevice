cc344e8083c7e8b41138ee8c81009bd8a4a53eae
Jo4096 <jcarlos.rebelo.lopes2004@gmail.com>
2025-11-20 15:50:16 +0000

    Refactor ComSimples example and add ComSimples2
    Refactored the ComSimples.ino example for improved readability and code structure, including better handler documentation and streamlined logic. Added a new ComSimples2.ino example demonstrating how to subscribe to another device's sensor data and control its LED based on a threshold.

--- examples/ComSimples/ComSimples.ino
+++ examples/ComSimples/ComSimples.ino
@@ -1,159 +1,92 @@
 #include "MqttDevice.hpp"
 
-
-
 const char* SSID = "SSID";
-
 const char* PASSWORD = "PASSWORD";
-
-
-
-const int PORT = 1883;
-
-
-
 const char* MQTT_BROKER = "test.mosquitto.org";
-
-
-
+const int PORT = 1883;
 const char* MQTT_USER = nullptr;
-
 const char* MQTT_PASSWORD = nullptr;
 
 
 
 #define LED_PIN 2
-
-
-
 MqttDevice device("schwarz");
 
 
-
-
-
+// Função que vai correr periodicamente para escrever valores no subtópico "sensor/data"
 void writeRandomValue()
-
 {
-
   int value = random() % 1000;
-
   String str = String(value);
-
   Serial.println("A escrever dados do sensor em sensor/data : "+str);
-
   device.publishSubTopic("sensor/data", str.c_str());
-
 }
 
+//------------------------------------- Handlers -------------------------------------//
 
+// Os handlers/callbacks são funções que vão ser corridas quando um evento for detectado.
+// No nosso caso o evento é a receção de uma mensagem num determinado tópico
 
 void msg_handler(const char* topic, const char* payload)
-
 {
-
   if(payload)
-
   {
-
     Serial.println(payload);
-
   }
-
   else
-
   {
-
     Serial.println("nullptr");
-
   }
-
 }
-
-
-
-
 
 void led_handler(const char* topic, const char* payload)
-
 {
-
   bool ledState = false;
-
   if(payload != nullptr && strcmp(payload, "ON") == 0)
-
   {
-
     ledState = true;
-
   }
-
   digitalWrite(LED_PIN, ledState);
-
 }
 
-
+//------------------------------------------------------------------------------------//
 
 void setup()
-
 {
-
   Serial.begin(115200);
-
   pinMode(LED_PIN, OUTPUT);
-
   delay(1000);
-
   Serial.println("A começar");
 
-
-
   device.on("msg", msg_handler);
-
   device.on("led/cmd", led_handler);
 
+  // como o tópico fornecido não começa por "devices/", a biblioteca assume que é um subtópico de "devices/schwarz"
+  // esta lambda vai correr sempre quando o writeRandomValue() é chamada, pois writeRandomValue() escreve para este tópico
   device.on("sensor/data", [](const char* topic, const char* payload)
-
   {
-
     Serial.println("Mensagem recebida no sensor data: " + String(payload));
-
   });
 
-//para confirmar que a mensagem foi escrita. Como o topico fornecido nao comeca por devices/ signfica que ele vai ouvir os seus topicos
-
-// se um dispositivo quer ouvir a mensagem enviada por este dispositivo ele tem de fazer: .on("devices/schwarz/sensor/data", ...)
-
-// ou se sabes que os dispositivos tem sensor e queres ouvir todos os topicos sensor/data fazes .on("devices/+/sensor/data", ...) para ser generico
-
-
+  // se um dispositivo quer ouvir a mensagem enviada por este dispositivo ele teria de fazer: .on("devices/schwarz/sensor/data", ...);
+  // se um dispositivo quer ouvir a mensagem enviada para "sensor/data" independentemente de que dispositivo for ele faria .on("devices/+/sensor/data", ...);
 
   device.begin(SSID, PASSWORD, MQTT_BROKER, PORT, MQTT_USER, MQTT_PASSWORD);
-
 }
 
 
 
 unsigned long lastTime = 0;
-
 void loop()
-
 {
-
   device.loop();
 
   unsigned long now = millis();
-
-
-
+  
+  //corre de 5000ms em 5000ms (5 segundos)
   if(now - lastTime >= 5000)
-
   {
-
     writeRandomValue();
-
     lastTime = now;
-
   }
-
 }
---
+++ examples/ComSimples2/ComSimples2.ino
@@ -0,0 +1,58 @@
+#include "MqttDevice.hpp"
+
+const char* SSID = "SSID";
+const char* PASSWORD = "PASS";
+const char* MQTT_BROKER = "test.mosquitto.org";
+const int PORT = 1883;
+const char* MQTT_USER = nullptr;
+const char* MQTT_PASSWORD = nullptr;
+
+
+MqttDevice device("himberee");
+
+//------------------------------------- Handlers -------------------------------------//
+void sensor_handler(const char* topic, const char* payload)
+{
+  if(!payload)
+  {
+    Serial.println("Algo de errado aconteceu com o sensor...");
+    return;
+  }
+
+  //converter de string para int
+  int sensor_value = atoi(payload);
+  const int threshold = 500;
+
+  Serial.printf("Valor lido: %d\n", sensor_value);
+
+  // Se o valor lido for superior a um threshold definido envia "ON" para o led/cmd e envia uma mensagem para ser printada no ecrã
+  if(sensor_value > threshold)
+  {
+    device.publish("devices/schwarz/led/cmd", "ON");
+    device.publish("devices/schwarz/msg", "Valor do sensor superior a threshold");
+  }
+  else
+  {
+    device.publish("devices/schwarz/led/cmd", "OFF");
+    device.publish("devices/schwarz/msg", "Valor do sensor inferior a threshold");
+  }
+}
+
+//------------------------------------------------------------------------------------//
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("A começar");
+
+  //esta função vai ser corrida quando o schwarz enviar os valores do sensor no sensor/data
+  device.on("devices/schwarz/sensor/data", sensor_handler);
+  device.begin(SSID, PASSWORD, MQTT_BROKER, PORT, MQTT_USER, MQTT_PASSWORD);
+}
+
+
+void loop()
+{
+  device.loop();
+}
